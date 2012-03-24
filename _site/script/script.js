c5829d6e20f14ba47208dd1d4f4e23f8f983fa7e
Exploria <randomor@gmail.com>
2012-03-24 14:57:31 -0500

    added tinypeeps to the page

--- _site/script/script.js
+++ _site/script/script.js
@@ -1,4 +1,43 @@
  $(function() {
+
+     //tinypeep code
+     var counter = 0;
+     var bodyWidth = $("body").width();
+     var justMoved = false;
+     var update = function(){
+         if (justMoved == true){return};
+         if (counter < 15) {
+             //add more peeps
+                //add a random bunch of peeps
+                var peepNumber = Math.floor(Math.random()*6);
+                var i=0;
+                for(i=0; i<peepNumber; i++){
+                    var leftPosition = Math.floor(Math.random()*bodyWidth);
+                    $("<div class='tinypeep top' style='left: "+ leftPosition +"px'><img src='/img/tinypeep.png' alt='Hi!' /></div>").prependTo("body").delay(3000, "myQueue").queue("myQueue", function(){ 
+                        $(this).addClass("out"); 
+                        }).dequeue("myQueue");
+                    counter++;
+                }
+             setTimeout(update, 4000);
+         }else{
+             //stop adding peep, maybe do something else fun and more active, since they know we are not there
+         }
+     }
+
+     setTimeout(update, 5000);
+
+     var timeout;
+     $(document).mousemove(function(e){
+         if (counter){
+             $(".tinypeep").addClass("hide").delay(1000, "myQueue").queue("myQueue", function(){ 
+  $(this).remove()}).dequeue("myQueue");
+             counter = 0;
+         };
+         justMoved = true;
+         clearTimeout(timeout);
+         timeout = setTimeout(function(){justMoved=false;update();}, 5000);
+   }); 
+     
    $("a[href=#twitter]").click(function() {
      $.scrollTo($('#twitter'),1200);
      $('#twitter').addClass('tip');
@@ -10,17 +49,15 @@
      return false;
  });    
 
-     $.getJSON("https://twitter.com/statuses/user_timeline/randomor.json?callback=?&count=5", function(tweets){
-	var $twitter = $("#twitter ul");
-	$.each(tweets, function(i, tweet){
-	  tweet = processTweet(tweet);
-	  $twitter.append(tweet);
-	})
-    })
-
+	//      $.getJSON("https://twitter.com/statuses/user_timeline/randomor.json?callback=?&count=5", function(tweets){
+	// var $twitter = $("#twitter ul");
+	// $.each(tweets, function(i, tweet){
+	//   tweet = processTweet(tweet);
+	//   $twitter.append(tweet);
+	// })
+	//     })
+	// 
      var processTweet = function(tweet){
-	 console.log("processing", tweet.text);
-
 	 tweetDate = processDate(tweet.created_at);
 	 linkedText = parseLink(tweet.text);
 
