2a4b04d21d4d7fcccbd1b44060b5cb4801906053
Shaomeng Zhang <randomor@gmail.com>
2013-08-30 16:50:30 -0500

    Removed twitter section, twitter api changes too often...

--- script/script.js
+++ script/script.js
@@ -13,8 +13,8 @@ $(function() {
             var i=0;
             for(i=0; i<peepNumber; i++){
                 var leftPosition = Math.floor(Math.random()*bodyWidth);
-                $("<div class='tinypeep top' style='left: "+ leftPosition +"px'><img src='/img/tinypeep.png' alt='Hi!' /></div>").prependTo("body").delay(3000, "myQueue").queue("myQueue", function(){ 
-                    $(this).addClass("out"); 
+                $("<div class='tinypeep top' style='left: "+ leftPosition +"px'><img src='/img/tinypeep.png' alt='Hi!' /></div>").prependTo("body").delay(3000, "myQueue").queue("myQueue", function(){
+                    $(this).addClass("out");
                     }).dequeue("myQueue");
                     counter++;
                 }
@@ -29,84 +29,20 @@ $(function() {
         var timeout;
         $(document).bind('mousemove scroll', function(e){
             if (counter){
-                $(".tinypeep").addClass("hide").delay(1000, "myQueue").queue("myQueue", function(){ 
+                $(".tinypeep").addClass("hide").delay(1000, "myQueue").queue("myQueue", function(){
                     $(this).remove()}).dequeue("myQueue");
                     counter = 0;
                 };
                 justMoved = true;
                 clearTimeout(timeout);
                 timeout = setTimeout(function(){justMoved=false;update();}, 5000);
-            }); 
-
-            $("a[href=#twitter]").click(function() {
-                $.scrollTo($('#twitter'),1200);
-                $('#twitter').addClass('tip');
-                return false;
             });
+
             $("a[href=#flickr]").click(function() {
                 $.scrollTo($('#flickr'),1200);
                 $('#twitter').addClass('tip');
                 return false;
-            });    
-
-            // $.getJSON("https://twitter.com/statuses/user_timeline/randomor.json?callback=?&count=5", function(tweets){
-            //     var $twitter = $("#twitter ul");
-            //     $.each(tweets, function(i, tweet){
-            //         tweet = processTweet(tweet);
-            //         $twitter.append("hi there");
-            //     })
-            // })
-
-            // test comment
-
-            $.ajax({
-                type:"GET",
-                dataType:"jsonp",
-                url:"http://search.twitter.com/search.json?q=from:randomor",
-                success:function(r){
-                    var $twitter = $("#twitter ul");
-                    var tweets = r.results;
-                    $.each(tweets, function(i, tweet){
-                        tweet = processTweet(tweet);
-                        $twitter.append(tweet);
-                    })
-                    $twitter.append('<a href="http://twitter.com/randomor/" class="twitter-link">MORE &gt;&gt;</a>')
-                }
             });
-
-
-            var processTweet = function(tweet){
-                tweetDate = processDate(tweet.created_at);
-                linkedText = parseLink(tweet.text);
-
-                var tweetText = '<li class="twitter-item"> <span class="twitter-timestamp"><abbr title="' + tweetDate + '">' + tweetDate + '</abbr></span> ' + linkedText + ' </li>';
-                return tweetText;
-            };
-
-            var processDate = function(date){
-                var d = new Date(date);
-
-                return d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear();
-            };
-
-            var parseLink = function(text){
-                // parse urls
-                text = text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9_-]+\.[A-Za-z0-9_:%&~\?\/.=-]+/g, function(url) {
-                    return url.link(url)
-                })
-                // parse usernames
-                text = text.replace(/[@]+[A-Za-z0-9_-]+/g, function(u) {
-                    var username = u.replace("@","")
-                    return u.link("http://twitter.com/"+username)
-                })
-                // parse hashtags
-                text = text.replace(/[#]+[A-Za-z0-9_-]+/g, function(t) {
-                    var tag = t.replace("#","%23")
-                    return t.link("http://search.twitter.com/search?q="+tag)
-                })
-                return text;
-            }   
-
         });
 
 
